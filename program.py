134e6068487a8064cd03d4f91a13f3a8099d6b9a
slavcho179 <slavidimitrov06@abv.bg>
2023-09-21 00:13:21 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,13 @@
-name = 'Penda'
-name2 = 'Sharo'
+row = 10
 
-both = name + name2
-print(both)
+for i in range(1, row + 1):
+    for j in range(1, i + 1):
+        print(j,end=' ')
+    print('')
+for i in range(row + 1):
+    print('*', end=' ')
+print()
+for i in range(row, 0, - 1):
+    for j in range(i, 0, - 1):
+        print(j,end=' ')
+    print('')
