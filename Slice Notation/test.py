fb87b4e5a80d80855f38747fc1e15931fae6b03a
slavcho179 <135168991+slavcho179@users.noreply.github.com>
2023-09-21 23:00:41 +0300

    Add files via upload

---
+++ Slice Notation/test.py
@@ -0,0 +1,23 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_sentence = "hellospark"
+
+subset1 = my_list[:3]
+print(subset1)
+
+subset2 = my_list[-2:]
+print(subset2)
+
+subset3 = my_list[::3]
+print(subset3)
+
+subsetx = my_sentence[0:9:1]
+print(subsetx)
+
+subsety = my_sentence[0:5]
+print(subsety)
+
+subsetz = my_list[-1:-3:-1]
+print(subsetz)
+
+subset = my_list[-1::-1]
+print(subset)
