e7c4d3fa4218dfa6c76fe6c077eba8725cec943b
Slavi Dimitrov <135168991+slavcho179@users.noreply.github.com>
2023-09-25 19:22:15 +0300

    Add files via upload

--- Data Types and Variables/party_profit.py
+++ Data Types and Variables/party_profit.py
@@ -6,17 +6,19 @@ coins = 0
 
 # Logic
 for day in range(1, days + 1):
-    if day == 10:
+    if day % 10 == 0:
         group -= 2
-    if day == 15:
+    if day % 15 == 0:
         group += 5
-    coins = coins + 50 - (group * 2)
-    if day == 3:
-        coins = coins - (group * 3)
-    if day == 5:
-        coins = coins + (20 * group) - (group * 2)
-
-each_companion = coins / group
+    if day % 3 == 0:
+        coins -= (group * 3)
+    if day % 5 == 0:
+        coins += group * 20
+        if day % 3 == 0:
+            coins -= group * 2
+    coins = coins + 50
+    coins -= group * 2
+each_companion = floor(coins / group)
 print(f"{group} companions received {each_companion} coins each.")
 
 # Print User output
