6f5063b41266e6e971982701bf565e33c5a312e0
Slavi Dimitrov <135168991+sldimitrov@users.noreply.github.com>
2023-12-20 18:43:25 +0200

    initialise a directory for data types and vars

---
+++ Fundamentals/DataStructures/Lists/Data Types and Variables/concat_names.py
@@ -0,0 +1,7 @@
+# Read User input
+first_name = input()
+second_name = input()
+delimiter = input()
+# Logic
+print(f'{first_name}{delimiter}{second_name}')
+# Print User output
---
+++ Fundamentals/DataStructures/Lists/Data Types and Variables/convert_m_to_km.py
@@ -0,0 +1,6 @@
+# Read User input
+distance_in_m = int(input())
+# Logic
+distance_in_km = distance_in_m / 1000
+print(f'{distance_in_km:.2f}')
+# Print User output
