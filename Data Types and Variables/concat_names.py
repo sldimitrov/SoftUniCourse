fdd0ce90110bc8468b6d70a49308d8999d1c9080
Slavi Dimitrov <135168991+sldimitrov@users.noreply.github.com>
2023-12-20 18:12:53 +0200

    Delete Data Types and Variables directory files

--- Data Types and Variables/concat_names.py
+++
@@ -1,7 +0,0 @@
-# Read User input
-first_name = input()
-second_name = input()
-delimiter = input()
-# Logic
-print(f'{first_name}{delimiter}{second_name}')
-# Print User output
--- Data Types and Variables/pounds_to_dollars.py
+++
@@ -1,7 +0,0 @@
-# Read User input
-british_pounds = int(input())
-# Logic
-dollars = british_pounds * 1.31
-print(f'{dollars:.3f}')
-
-# Print User output
