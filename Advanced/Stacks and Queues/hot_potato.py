12d9ea8ede5458dd6cccc05b0ca47ae9146fd001
slavcho179 <slavidimitrov06@abv.bg>
2024-01-11 20:37:39 +0200

    add a try to solve an exercise

--- Advanced/Stacks and Queues/hot_potato.py
+++ Advanced/Stacks and Queues/hot_potato.py
@@ -3,8 +3,17 @@ from collections import deque
 queue = deque(input().split())
 
 # Read User Input
+# Tracy Emily Daniel
+# 2
+
+moves = int(input())
+queue.reverse()
 
 # Logic
-print(*queue)
+while len(queue) > 1:
+    queue.rotate(moves)
+    print(f"Removed {queue.popleft()}")
+    queue.rotate(1)
 
 # Print User output
+print(f"Last is {''.join(queue)}")
