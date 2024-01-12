05552ff56de2fc5f7a677e5d132c683e630142fd
slavcho179 <slavidimitrov06@abv.bg>
2024-01-12 17:01:24 +0200

    upload a solution for the problem

---
+++ Advanced/Stacks and Queues/stacked_queries.py
@@ -0,0 +1,20 @@
+# User input
+n = int(input())
+
+stack = []
+
+# Logic
+for n in range(n):
+    line = input().split()
+    if line[0] == '1':
+        stack.append(int(line[1]))
+    elif line[0] == '2':
+        stack.pop()
+    elif line[0] == '3':
+        print(max(stack))
+    elif line[0] == '4':
+        print(min(stack))
+
+# Print User output
+print(*reversed(stack), sep=', ')
+
