2db4e6b056438008dc89ff1744b977235da345b4
slavcho179 <slavidimitrov06@abv.bg>
2024-01-12 22:33:22 +0200

    write the code couple of times to memorize it

--- Advanced/Stacks and Queues/balanced_parentheses.py
+++ Advanced/Stacks and Queues/balanced_parentheses.py
@@ -1,18 +1,18 @@
 from collections import deque
 
 parentheses = deque(input())
-open_parentheses = deque
+opening_parentheses = deque()
 
 while parentheses:
     current_parentheses = parentheses.popleft()
 
-    if current_parentheses in "([{":
-        open_parentheses.append(current_parentheses)
-    elif not open_parentheses:
+    if current_parentheses in "({[":
+        opening_parentheses.append(current_parentheses)
+    elif not opening_parentheses:
         print("NO")
         break
     else:
-        if f"{open_parentheses.pop + current_parentheses}" not in "{}[]()":
+        if f"{opening_parentheses.pop() + current_parentheses}" not in "(){}[]":
             print("NO")
             break
 
