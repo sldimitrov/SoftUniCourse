fe8ccd1b5d8f25c97b0f120bf32d8c5f3e3a65e5
slavcho179 <slavidimitrov06@abv.bg>
2024-01-12 17:04:27 +0200

    Compare the difference in the time of termination of two algorithms

--- Advanced/Stacks and Queues/reverse_numbers.py
+++ Advanced/Stacks and Queues/reverse_numbers.py
@@ -1,5 +1,13 @@
+import datetime
+
+start = datetime.datetime.now()
 # Read User input
 numbers = input().split()
 
 # Print User output
 print(*reversed(numbers))
+
+end = datetime.datetime.now()
+
+print(end - start)
+# Taken time - 0:00:01.678697
