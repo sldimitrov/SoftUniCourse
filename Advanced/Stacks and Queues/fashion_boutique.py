186bfc3622baa1159e6dbc1e8230264f0c44713f
slavcho179 <slavidimitrov06@abv.bg>
2024-01-12 18:53:38 +0200

    upload the final solution of the last exercise

--- Advanced/Stacks and Queues/fashion_boutique.py
+++ Advanced/Stacks and Queues/fashion_boutique.py
@@ -1,24 +1,19 @@
-racks_needed = 1
-
 # Read User input
-box_of_clothes = [int(x) for x in input().split()]
-rack_capacity = int(input())
+clothes = [int(x) for x in input().split()]
+rack_space = int(input())
+
+racks_needed = 1
+current_rack_space = rack_space
 
 # Logic
+while clothes:
+    cloth = clothes.pop()
 
-# Free space
-current_rack = rack_capacity
-while box_of_clothes:
-    # Space required
-    current_cloth = box_of_clothes.pop()
-
-    # If the required space is < or = than the free space:
-    if current_cloth < current_rack:
-        current_rack -= current_cloth  # decrease the free space
+    if current_rack_space >= cloth:
+        current_rack_space -= cloth
     else:
-        # if the space required is more than the free one
-        racks_needed += 1  # add a rack
-        current_rack = rack_capacity
+        racks_needed += 1
+        current_rack_space = rack_space - cloth
 
 # Print User output
 print(racks_needed)
