1eb672d4e31a51dc4b86506bc139a0c297d10522
slavcho179 <slavidimitrov06@abv.bg>
2024-01-18 00:21:47 +0200

    upload binary_search algorithm

--- Advanced/Algorithms/binary_search.py
+++ Advanced/Algorithms/binary_search.py
@@ -1,4 +1,5 @@
-def binarySearch(lst, x):
+
+def binary_search(lst: list, x: str):
     low = 0
     high = len(lst)-1
     # Repeat until the pointers low and high meet each other
@@ -16,3 +17,8 @@ def binarySearch(lst, x):
             high = mid - 1
 
     return -1
+
+
+list_of_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+y = 7
+print(binary_search(list_of_nums, y))
