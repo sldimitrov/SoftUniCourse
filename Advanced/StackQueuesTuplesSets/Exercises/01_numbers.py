d5e1ba55531b48957a9dbec0bff2c59901e90df8
slavcho179 <slavidimitrov06@abv.bg>
2024-01-19 21:03:17 +0200

    fix a bug within a solution

--- Advanced/StackQueuesTuplesSets/Exercises/01_numbers.py
+++ Advanced/StackQueuesTuplesSets/Exercises/01_numbers.py
@@ -1,6 +1,6 @@
 # Read User input
-first_set = set([x for x in input().split()])
-second_set = set([x for x in input().split()])
+first_set = set([int(x) for x in input().split()])
+second_set = set([int(x) for x in input().split()])
 
 # Logic
 for i in range(int(input())):  # n times
@@ -10,21 +10,25 @@ for i in range(int(input())):  # n times
 
     if command == "Add First":
         for el in numbers:
+            el = int(el)
             first_set.add(el)
     elif command == "Add Second":
         for el in numbers:
+            el = int(el)
             second_set.add(el)
     elif command == "Remove First":
         for el in numbers:
+            el = int(el)
             if el in first_set:
                 first_set.remove(el)
     elif command == "Remove Second":
         for el in numbers:
-            if el in first_set:
+            el = int(el)
+            if el in second_set:
                 second_set.remove(el)
     elif command == "Check Subset":  # Example - if a contains b print True
         print(first_set.issubset(second_set) or second_set.issubset(first_set))
 
 # Print User output
-print(", ".join(sorted(first_set)))
-print(", ".join(sorted(second_set)))
+print(", ".join(sorted([str(x) for x in first_set])))
+print(", ".join(sorted([str(x) for x in second_set])))
