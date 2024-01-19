d69e5309b3d186de392354af6bcfcafee54cee28
slavcho179 <slavidimitrov06@abv.bg>
2024-01-19 23:09:52 +0200

    upload final solution of the exercise

--- Advanced/StackQueuesTuplesSets/Exercises/01_numbers.py
+++ Advanced/StackQueuesTuplesSets/Exercises/01_numbers.py
@@ -1,34 +1,24 @@
 # Read User input
-first_set = set([int(x) for x in input().split()])
-second_set = set([int(x) for x in input().split()])
+first_set = set(int(x) for x in input().split())
+second_set = set(int(x) for x in input().split())
 
 # Logic
 for i in range(int(input())):  # n times
-    action, sequence, *numbers = input().split()
-    # concatenate sequence and action together
+    action, sequence, *data = input().split()
+
     command = action + ' ' + sequence
 
     if command == "Add First":
-        for el in numbers:
-            el = int(el)
-            first_set.add(el)
+        [first_set.add(int(el)) for el in data]
     elif command == "Add Second":
-        for el in numbers:
-            el = int(el)
-            second_set.add(el)
+        [second_set.add(int(el)) for el in data]
     elif command == "Remove First":
-        for el in numbers:
-            el = int(el)
-            if el in first_set:
-                first_set.remove(el)
+        [first_set.discard(int(el)) for el in data]
     elif command == "Remove Second":
-        for el in numbers:
-            el = int(el)
-            if el in second_set:
-                second_set.remove(el)
+        [second_set.discard(int(el)) for el in data]
     elif command == "Check Subset":  # Example - if a contains b print True
         print(first_set.issubset(second_set) or second_set.issubset(first_set))
 
 # Print User output
-print(", ".join(sorted([str(x) for x in first_set])))
-print(", ".join(sorted([str(x) for x in second_set])))
+print(*sorted(first_set), sep=", ")
+print(*sorted(second_set), sep=", ")
