4f9e0a2eb4cdecf07076f1465efcafb3e8bb4ad9
slavcho179 <slavidimitrov06@abv.bg>
2024-01-20 17:51:50 +0200

    re-write a solution in order to earn new knowledge #2

--- Advanced/StackQueuesTuplesSets/Exercises/01_numbers_2.py
+++ Advanced/StackQueuesTuplesSets/Exercises/01_numbers_2.py
@@ -2,21 +2,18 @@ first_set = set(int(x) for x in input().split())
 second_set = set(int(x) for x in input().split())
 
 functions = {
-    "Add First": lambda x: [first_set.add(int(el)) for el in x],
-    "Add Second": lambda x: [second_set.add(int(el)) for el in x],
-    "Remove First": lambda x: [first_set.discard(int(el)) for el in x],
-    "Remove Second": lambda x: [second_set.discard(int(el)) for el in x],
-    "Check Subset": lambda x: print(first_set.issubset(second_set) or second_set.issubset(first_set)),
+    "Add First": lambda x: [first_set.add(int(i)) for i in x],
+    "Add Second": lambda x: [second_set.add(int(i)) for i in x],
+    "Remove First": lambda x: [first_set.discard(int(i)) for i in x],
+    "Remove Second": lambda x: [second_set.discard(int(i)) for i in x],
+    "Check Subset": lambda x: [print(first_set.issubset(second_set) or second_set.issubset(first_set))],
 }
 
-for _ in range(int(input())):
-    first_command, second_command, *data = input().split()
-
-    command = first_command + " " + second_command
+for i in range(int(input())):
+    action, number, *data = input().split()
+    command = action + ' ' + number
 
     functions[command](data)
 
-print(*sorted(first_set), sep=", ")
-print(*sorted(second_set), sep=", ")
-
-
+print(*sorted(first_set), sep=', ')
+print(*sorted(second_set), sep=', ')
