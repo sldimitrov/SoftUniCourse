b3d248e45f5a81c34a542f770287df18711c207b
slavcho179 <slavidimitrov06@abv.bg>
2024-01-20 18:24:33 +0200

    add comments

--- Advanced/StackQueuesTuplesSets/Exercises/02_expression_evaluator.py
+++ Advanced/StackQueuesTuplesSets/Exercises/02_expression_evaluator.py
@@ -1,10 +1,13 @@
 from collections import deque
 from math import floor
 
+# Read User input
 expression = deque(input().split())
 
+# Initialize an index counter
 idx = 0
 
+# Logic
 while idx < len(expression):
     element = expression[idx]
     if element == "*":
@@ -20,9 +23,11 @@ while idx < len(expression):
         for i in range(idx - 1):
             expression.appendleft(int(expression.popleft()) / int(expression.popleft()))
 
+    # Check if next element is symbol and remove it from expression
     if expression[1] in "*+-/":
         del expression[1]
         idx = 1
     idx += 1
 
+# Print User output
 print(floor(expression[0]))
