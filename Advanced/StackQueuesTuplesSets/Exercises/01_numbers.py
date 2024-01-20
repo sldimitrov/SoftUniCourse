028cae12a5c7247fefccaa146fbb0865b0bd33b8
slavcho179 <slavidimitrov06@abv.bg>
2024-01-20 17:44:29 +0200

    re-write a solution in order to earn new knowledge

--- Advanced/StackQueuesTuplesSets/Exercises/01_numbers.py
+++ Advanced/StackQueuesTuplesSets/Exercises/01_numbers.py
@@ -3,20 +3,18 @@ second_set = set(int(x) for x in input().split())
 
 for i in range(int(input())):
     action, number, *data = input().split()
-
     command = action + ' ' + number
+
     if command == "Add First":
-        [first_set.add(int(el)) for el in data]
+        [first_set.add(int(x)) for x in data]
     elif command == "Add Second":
-        [second_set.add(int(el)) for el in data]
+        [second_set.add(int(x)) for x in data]
     elif command == "Remove First":
-        [first_set.discard(int(el)) for el in data]
+        [first_set.discard(int(x)) for x in data]
     elif command == "Remove Second":
-        [second_set.discard(int(el)) for el in data]
+        [second_set.discard(int(x)) for x in data]
     elif command == "Check Subset":
         print(first_set.issubset(second_set) or second_set.issubset(first_set))
 
-print(*sorted(first_set), sep=", ")
-print(*sorted(second_set), sep=", ")
-
-
+print(*sorted(first_set), sep=', ')
+print(*sorted(second_set), sep=', ')
