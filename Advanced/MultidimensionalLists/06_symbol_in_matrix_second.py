4b2139519358bd94753a3256db29fc805dc5b44d
slavcho179 <slavidimitrov06@abv.bg>
2024-01-21 19:52:17 +0200

    add comments

--- Advanced/MultidimensionalLists/06_symbol_in_matrix_second.py
+++ Advanced/MultidimensionalLists/06_symbol_in_matrix_second.py
@@ -1,22 +1,23 @@
+# Read the size of the square matrix
 size = int(input())
 
+# Initialise a list
 matrix = []
 
+# Add data to the matrix
 for i in range(size):
     matrix.append(input())
 
+# Read the searched element
 searched_element = input()
 
-is_found = False
-
+# Search for the element in the matrix
 for row_index in range(size):
     for col_index in range(size):
         if matrix[row_index][col_index] == searched_element:
             print(f"({row_index}, {col_index})")
-            break
-    if is_found:
-        break
+            exit()  # exit if found
 
-if not is_found:
-    print(f"{searched_element} does not occur in the matrix")
+# else print:
+print(f"{searched_element} does not occur in the matrix")
 
