8a05445c7722378e6ef8905239774d184bd6b905
slavcho179 <slavidimitrov06@abv.bg>
2024-01-21 17:11:29 +0200

    add exercise

---
+++ Advanced/MultidimensionalLists/find_index.py
@@ -0,0 +1,10 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Print each element using the index
+for row_index in range(len(matrix)):
+    for col_index in range(len(matrix[row_index])):
+        print(matrix[row_index][col_index])
