586ff61ed922f0314f3e9261d182dd955948b75e
slavcho179 <slavidimitrov06@abv.bg>
2024-01-21 16:16:49 +0200

    add solution

--- Advanced/MultidimensionalLists/03_flattering_matrix.py
+++ Advanced/MultidimensionalLists/03_flattering_matrix.py
@@ -7,6 +7,9 @@ matrix = []
 # Logic
 for i in range(n_rows):
     line = [int(x) for x in input().split(', ')]
+    matrix.append(line)
+
+flatten = [el for row in matrix for el in row]
 
 # Print User output
-print(matrix)
+print(flatten)
