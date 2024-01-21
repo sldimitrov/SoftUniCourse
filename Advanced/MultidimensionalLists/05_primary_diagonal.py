1eebe771550f49fc2422e68e9df52772c18f24d6
slavcho179 <slavidimitrov06@abv.bg>
2024-01-21 19:05:09 +0200

    add comments

--- Advanced/MultidimensionalLists/05_primary_diagonal.py
+++ Advanced/MultidimensionalLists/05_primary_diagonal.py
@@ -1,14 +1,19 @@
+# Read User input
 row = int(input())
 
+# Initialise matrix
 matrix = []
 
+# Read data for the matrix
 for _ in range(row):
     matrix.append([int(el) for el in input().split()])
 
+# Initialise a counter
 diagonal_sum = 0
-for row_index in range(row):
-    for col_index in range(row):
-        if row_index == col_index:
-            diagonal_sum += matrix[row_index][col_index]
 
+# Sum diagonal
+for row_index in range(row):
+    diagonal_sum += matrix[row_index][row_index]
+
+# Print User output
 print(diagonal_sum)
