73e3cfd8ff76c048ef3a47fa5ff7d157fc04ce39
slavcho179 <slavidimitrov06@abv.bg>
2024-01-22 23:24:40 +0200

    add an additional solution for an exercise

--- Advanced/MultidimensionalLists/Lab/07_n_square_with_max_sum.py
+++ Advanced/MultidimensionalLists/Lab/07_n_square_with_max_sum.py
@@ -1,5 +1,6 @@
 # Read User input
 n_rows, n_cols = [int(x) for x in input().split(', ')]
+# The size of the square we will search within
 size = int(input())
 matrix = []
 
@@ -11,24 +12,27 @@ for i in range(n_rows):
     matrix.append(sub_list)
 
 biggest_sum = float('-inf')
-sub_matrix = []  # and sub matrix
+highest_score_matrix = []
 
 # Iterate through the valid range of the matrix
 for row in range(n_rows - (size - 1)):
-    for col in range(n_cols - size):
-        # Initialise variables for the square corners
-        current_space = matrix[row][col]
-        right_space = matrix[row][col + 1]
-        down_space = matrix[row +1][col]
-        diagonal_space = matrix[row +1][col + 1]
-        # Calculate the sum of its numbers
-        current_sum = current_space + right_space + down_space + diagonal_space
+    for col in range(n_cols - (size - 1)):
+        # Initialise variables for the sub matrix
+        current_cell = matrix[row][col]
+        sub_matrix = []
+        current_sum = 0
+        for r in range(size):
+            matrix_line = []
+            for c in range(size):
+                matrix_line.append(matrix[r][c])
+                current_sum += matrix[r][c]
+            sub_matrix.append(matrix_line)
+
         if current_sum > biggest_sum:
             biggest_sum = current_sum
-            sub_matrix = [[current_space, right_space], [down_space, diagonal_space]]
+            highest_score_matrix = sub_matrix
 
 # Print User output
-for matrix in sub_matrix:
+for matrix in highest_score_matrix:
     print(*matrix)
 print(biggest_sum)
-# TODO: find a way to make it work with n square
