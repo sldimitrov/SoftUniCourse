04c6deba120677d4e05ffa46646719c27fb56329
slavcho179 <slavidimitrov06@abv.bg>
2024-01-23 20:26:24 +0200

    update a solution

--- Advanced/MultidimensionalLists/Exercises/02_diagonal_difference.py
+++ Advanced/MultidimensionalLists/Exercises/02_diagonal_difference.py
@@ -1,13 +1,26 @@
-# Initialization of the matrix
+# # Initialization of the matrix
+# n = int(input())
+# matrix = [[int(c) for c in input().split()] for r in range(n)]
+#
+# # Find the sum of diagonal values
+# first = 0
+# second = 0
+# for r in range(n):
+#     first += matrix[r][r]
+#     second += matrix[r][n - r - 1]
+#
+# # Print User output
+# print(abs(first - second))
+
+
+# sol 2
 n = int(input())
-matrix = [[int(c) for c in input().split()] for r in range(n)]
 
-# Find the sum of diagonal values
-first = 0
-second = 0
-for r in range(n):
-    first += matrix[r][r]
-    second += matrix[r][n - r - 1]
+primary, secondary = 0, 0
 
-# Print User output
-print(abs(first - second))
+for i in range(n):
+    line = [int(x) for x in input().split()]
+    primary += line[i]
+    secondary += line[n - i - 1]
+
+print(abs(primary - secondary))
