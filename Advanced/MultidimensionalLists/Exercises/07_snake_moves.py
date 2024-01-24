8df7af2774bc70f78a05f45a5759d676e683a4bd
slavcho179 <slavidimitrov06@abv.bg>
2024-01-24 20:00:55 +0200

    re-write a solution

--- Advanced/MultidimensionalLists/Exercises/07_snake_moves.py
+++ Advanced/MultidimensionalLists/Exercises/07_snake_moves.py
@@ -1,15 +1,18 @@
 from collections import deque
 
+# Read User input
 rows, cols = [int(x) for x in input().split()]
 
+# Initialise collections
 word = list(input())
-
 word_copy = deque(word)
 
+# Logic
 for row in range(rows):
     while len(word_copy) < cols:
         word_copy.extend(word)
 
+    # Print User output
     if row % 2 == 0:
         print(*[word_copy.popleft() for _ in range(cols)], sep='')
     else:
