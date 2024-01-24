2fd51b22a7efd4eb42d5e8e4474b344e856f82ed
slavcho179 <slavidimitrov06@abv.bg>
2024-01-24 21:27:19 +0200

    add a solution of an exercise about matrixes

--- Advanced/MultidimensionalLists/Exercises/09_miner.py
+++ Advanced/MultidimensionalLists/Exercises/09_miner.py
@@ -1,28 +1,55 @@
-# Read User data
-size = int(input())
+# Read User input
+n = int(input())
 commands = input().split()
 
-# Initialize
-matrix = []  # the matrix
-player_coords = []  # player coordinates
-total_coal, current_coal = 0, 0  # counters
-
-moves = {
-    "left": [0, -1],
-    "right": [0, 1],
+# Initialise directions
+directions = {
     "up": [-1, 0],
-    "down": [1, 0]
+    "down": [1, 0],
+    "right": [0, 1],
+    "left": [0, -1]
 }
 
-# Matrix logic
-for r in range(size):
-    line = input().split()
-    if 's' in line:  # find player's position
-        player_coords = [r, line.index('s')]
-        row, col = player_coords
-    if 'c' in line:  # count down the coal
-        total_coal += line.count('c')
-    matrix.append(line)
+# Initialise
+matrix = []  # the matrix
+player_pos = []  # player position
+total_coal, collected_coal = 0, 0  # counters
 
-# Move around the matrix
-for command in commands:
+# Logic
+for row in range(n):  # Matrix
+    matrix.append(input().split())
+
+    if "s" in matrix[row]:  # player pos
+        r, c = row, matrix[row].index("s")
+        player_pos = [r, c]
+        matrix[r][c] = '*'
+
+    if "c" in matrix[row]:  # coal count
+        total_coal += matrix[row].count("c")
+
+# Player moves
+for m in commands:
+    # Calculate new coordinates for the player
+    r, c = player_pos[0] + directions[m][0], player_pos[1] + directions[m][1]
+
+    # Validate these new coordinates
+    if not (0 <= r < n and 0 <= c < n):
+        continue  # if not valid
+
+    # Change the position of the player
+    player_pos = [r, c]
+
+    if matrix[r][c] == "c":  # if mining
+        collected_coal += 1
+        if collected_coal == total_coal:  # if won
+            print(f"You collected all coal! ({player_pos[0]}, {player_pos[1]})")
+            break
+
+    if matrix[r][c] == "e":  # if leaving
+        print(f"Game over! ({player_pos[0]}, {player_pos[1]})")
+        break
+
+    matrix[r][c] = "*"
+
+else:  # if lost
+    print(f"{total_coal - collected_coal} pieces of coal left. ({player_pos[0]}, {player_pos[1]})")
