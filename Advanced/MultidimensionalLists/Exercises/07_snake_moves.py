c6b2b0ef8f276dde0248573830b979f0f1fc9aa2
slavcho179 <slavidimitrov06@abv.bg>
2024-01-24 19:56:17 +0200

    upload a solution of an exercise and initialise the file for the next one

--- Advanced/MultidimensionalLists/Exercises/07_snake_moves.py
+++ Advanced/MultidimensionalLists/Exercises/07_snake_moves.py
@@ -1,18 +1,15 @@
 from collections import deque
 
-# Read User data
 rows, cols = [int(x) for x in input().split()]
+
 word = list(input())
 
-# Initialise a queue
 word_copy = deque(word)
 
-# Logic
 for row in range(rows):
     while len(word_copy) < cols:
         word_copy.extend(word)
 
-    # Print snake lines
     if row % 2 == 0:
         print(*[word_copy.popleft() for _ in range(cols)], sep='')
     else:
