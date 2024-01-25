f8f45c487417c46185eed033218099d249376484
slavcho179 <slavidimitrov06@abv.bg>
2024-01-26 00:25:25 +0200

    upload a solution of an exercise

--- Advanced/MultidimensionalLists/MoreExercises/flatten_lists.py
+++ Advanced/MultidimensionalLists/MoreExercises/flatten_lists.py
@@ -1,5 +1,16 @@
-# Read data and initialise the matrix
-matrix = [[int(x) for x in i.split()] for i in input().split('|')]
+# # Read data and initialise the matrix
+# matrix = [[int(x) for x in i.split()] for i in input().split('|')]
+#
+# # Print the matrix backwards
+# [print(*sub_matrix, end=' ') for sub_matrix in matrix[::-1]]
 
-# Print the matrix backwards
-[print(*sub_matrix, sep=' ') for sub_matrix in matrix[::-1]]
+
+# solution 2
+line = input().split('|')
+
+sub_lists = []
+
+for el in line[::-1]:
+    sub_lists.extend(el.split())
+
+print(*sub_lists)
