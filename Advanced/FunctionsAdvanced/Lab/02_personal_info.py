38551b2d9f0af73e5f74ddb4b90585e38343e417
slavcho179 <slavidimitrov06@abv.bg>
2024-01-27 15:32:31 +0200

    upload a solution

---
+++ Advanced/FunctionsAdvanced/Lab/02_personal_info.py
@@ -0,0 +1,7 @@
+# Defien a function
+def get_info(name, age, town):
+    return f"This is {name} from {town} and he is {age} years old"
+
+
+# Call the function | unpack the arguments, passing them to the function
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
