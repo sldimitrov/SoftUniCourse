65ebf7809b5eb506457fa167844b43c0f529d0cf
slavcho179 <slavidimitrov06@abv.bg>
2024-01-27 14:38:30 +0200

    upload function definition

---
+++ Advanced/FunctionsAdvanced/Lab/main.py
@@ -0,0 +1,10 @@
+# Defining a function
+def count_num(end_num):
+    """
+    Print out numbers from 1 to given range
+    """
+    for num in range(1, end_num+1):
+        print(num)
+
+# Calling the function
+count_num(12)
