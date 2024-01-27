88afb69615ceddd28f8082e6be548132c938f8ef
slavcho179 <slavidimitrov06@abv.bg>
2024-01-27 18:58:17 +0200

    upload a solution of an exercise

--- Advanced/FunctionsAdvanced/Lab/04_rectangle.py
+++ Advanced/FunctionsAdvanced/Lab/04_rectangle.py
@@ -1,14 +1,20 @@
+# Define a function
 def rectangle(length, width):
-    if not isinstance(length, int) or not isinstance(width, int):
+    # Check if both params are int, else : error
+    if not (isinstance(length, int) and isinstance(width, int)):
         return "Enter valid values!"
 
+    # Define an inner function
     def area():
-        return length * width
+        return length*width
 
+    # Define a nested function
     def perimeter():
-        return (length + width) * 2
+        return 2*(length+width)
 
+    # Calling the 2 inner functions within the f.form., returns a message
     return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
 
 
+# Call the main function and print out the returned message
 print(rectangle(2, 10))
