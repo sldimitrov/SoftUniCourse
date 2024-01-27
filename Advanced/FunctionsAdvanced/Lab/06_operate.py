08e5fd1dc36ff83326e3f2063ad501d0d545c5ca
slavcho179 <slavidimitrov06@abv.bg>
2024-01-27 20:18:02 +0200

    upload a solution of a problem

--- Advanced/FunctionsAdvanced/Lab/06_operate.py
+++ Advanced/FunctionsAdvanced/Lab/06_operate.py
@@ -2,12 +2,18 @@ from functools import reduce
 
 
 # Define a function
+# def operate(operator, *args):
+#     if operator == "+":
+#         return reduce(lambda x, y: x+y, args)
+#     elif operator == "-":
+#         return reduce(lambda x, y: x-y, args)
+#     elif operator == "*":
+#         return reduce(lambda x,y: x-y, args)
+#     elif operator == "/":
+#         return reduce(lambda x,y: x/y, args)
+
+
+# do NOT do use in real-live-project
+# be aware of dropping the data base
 def operate(operator, *args):
-    if operator == "+":
-        return reduce(lambda x, y: x+y, args)
-    elif operator == "-":
-        return reduce(lambda x, y: x-y, args)
-    elif operator == "*":
-        return reduce(lambda x,y: x-y, args)
-    elif operator == "/":
-        return reduce(lambda x,y: x/y, args)
+    return reduce(lambda x,y: eval(f"{x}{operator}{y}"), args)
