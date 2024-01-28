7cba7b677e5db8d6010332247ab5d21d34dbae23
slavcho179 <slavidimitrov06@abv.bg>
2024-01-28 18:00:11 +0200

    add a solution

--- Advanced/FunctionsAdvanced/Exercises/07_grocery.py
+++ Advanced/FunctionsAdvanced/Exercises/07_grocery.py
@@ -1,8 +1,20 @@
 # Define a function
-def grocery_store():
-    pass
+def grocery_store(**kwargs):
+    kwargs = sorted(kwargs.items(), key=lambda kvp: (-kvp[1], -len(kvp[0]), kvp[0]))
+
+    result = []
+
+    for k, v in kwargs:
+        result.append(f"{k}: {v}")
+
+    return "\n".join(result)
 
 
 # Test Code
+print(grocery_store(
+    bread=5,
+    pasta=12,
+    eggs=12,
+))
 
 
