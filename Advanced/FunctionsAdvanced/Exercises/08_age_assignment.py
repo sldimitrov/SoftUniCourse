96bd34e149c7d3a19a839f87ff39dce9dc2ffd21
slavcho179 <slavidimitrov06@abv.bg>
2024-01-29 00:35:35 +0200

    fix bug

--- Advanced/FunctionsAdvanced/Exercises/08_age_assignment.py
+++ Advanced/FunctionsAdvanced/Exercises/08_age_assignment.py
@@ -2,13 +2,13 @@
 def age_assignment(*names, **age_data):
     result = []
 
-    for name in names:
-        for letter, age in age_data.items():
+    for letter, age in age_data.items():
+        for name in names:
             if name.startswith(letter):
                 result.append(f"{name} is {age} years old.")
                 break
 
-    return '\n'.join(result)
+    return '\n'.join(sorted(result))
 
 
 # Test code
