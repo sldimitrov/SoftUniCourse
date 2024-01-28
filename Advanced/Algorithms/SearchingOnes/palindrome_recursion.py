efa12359fe9e5ffa1db1ba4af0249d7e585faf53
slavcho179 <slavidimitrov06@abv.bg>
2024-01-29 00:44:25 +0200

    add comments

--- Advanced/Algorithms/SearchingOnes/palindrome_recursion.py
+++ Advanced/Algorithms/SearchingOnes/palindrome_recursion.py
@@ -1,3 +1,4 @@
+# Define a function
 def palindrome(word, idx):
     if idx == len(word) // 2:
         return f"{word} is a palindrome"
@@ -5,7 +6,9 @@ def palindrome(word, idx):
     if word[idx] != word[-idx-1]:
         return f"{word} is not a palindrome"
 
-    return palindrome(word, idx+1)
+    return palindrome(word, idx + 1)
 
 
+# Test Code
 print(palindrome("abcba", 0))
+print(palindrome("peter", 0))
