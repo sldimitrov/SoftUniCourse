90dc2edc4bfd0c6ed5f3024d19eae2828a80bcb3
slavcho179 <slavidimitrov06@abv.bg>
2024-01-29 00:37:35 +0200

    add an algorithm

---
+++ Advanced/Algorithms/SearchingOnes/palindrome_recursion.py
@@ -0,0 +1,11 @@
+def palindrome(word, idx):
+    if idx == len(word) // 2:
+        return f"{word} is a palindrome"
+
+    if word[idx] != word[-idx-1]:
+        return f"{word} is not a palindrome"
+
+    return palindrome(word, idx+1)
+
+
+print(palindrome("abcba", 0))
