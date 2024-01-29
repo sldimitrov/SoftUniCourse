1c4ab0452b6f0536bb40d78613845222bac8474f
slavcho179 <slavidimitrov06@abv.bg>
2024-01-29 21:02:48 +0200

    upload a text file

---
+++ test.py
@@ -0,0 +1,8 @@
+import time
+
+
+name = input('Please input your name: ')
+time.sleep(1)
+age = input('Please input your age: ')
+time.sleep(2)
+print(f"\nHello {name.capitalize()}, {age}-years old human-being!")
