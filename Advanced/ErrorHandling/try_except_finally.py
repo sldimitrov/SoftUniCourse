840d9647730d94772051d2c486eb9f83b75a38cf
slavcho179 <slavidimitrov06@abv.bg>
2024-01-29 22:08:40 +0200

    update file

--- Advanced/ErrorHandling/try_except_finally.py
+++ Advanced/ErrorHandling/try_except_finally.py
@@ -3,8 +3,8 @@ number = int(input('Number: '))
 try:
     a = 10 / number
     print('Valid division!')
-except ZeroDivisionError:
-    print('Zero division')
+except ZeroDivisionError as zd:
+    print(zd)
 
 finally:
     print('Final result...')
