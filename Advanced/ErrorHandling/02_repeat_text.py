81b8fc0617f7e2a1dedc966f17b26653331c9bb9
slavcho179 <slavidimitrov06@abv.bg>
2024-01-30 18:29:10 +0200

    add a solution for the exercise

---
+++ Advanced/ErrorHandling/02_repeat_text.py
@@ -0,0 +1,9 @@
+# Read User input
+text = input()
+
+# Scenarios
+try:  # input an integer number
+    multiplier = int(input())
+    print(text*multiplier)
+except ValueError:  # if not
+    print('Variable must be an integer')
---
+++ Advanced/ErrorHandling/03_value_cannot_be_negative.py
@@ -0,0 +1,10 @@
+# Initialise a class to be used as an exception
+class ValueCannotBeNegative(Exception):
+    pass
+
+
+while True:
+    number = int(input())
+    if number < 0:  # if number is negative
+        # throws an exception and stops the program
+        raise ValueCannotBeNegative
