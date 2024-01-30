44f089257fb51af645c35fcece09f2dbaa74c58b
slavcho179 <slavidimitrov06@abv.bg>
2024-01-30 18:20:07 +0200

    add comments

--- Advanced/ErrorHandling/01_so_many_exceptions.py
+++ Advanced/ErrorHandling/01_so_many_exceptions.py
@@ -1,6 +1,8 @@
+# Read User input
 numbers_list = [int(x) for x in input().split(', ')]
 result = 1
 
+# Iterate through the indexes
 for i in range(len(numbers_list)):
     number = numbers_list[i]
     if number <= 5:
@@ -8,4 +10,5 @@ for i in range(len(numbers_list)):
     elif 5 < number <= 10:
         result /= number
 
+# Print User output
 print(result)
