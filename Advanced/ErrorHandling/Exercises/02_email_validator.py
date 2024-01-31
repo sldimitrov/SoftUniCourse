99a156a1e5cd2c15415817562658a0f3267bede0
slavcho179 <slavidimitrov06@abv.bg>
2024-01-31 19:48:26 +0200

    update project

--- Advanced/ErrorHandling/Exercises/02_email_validator.py
+++ Advanced/ErrorHandling/Exercises/02_email_validator.py
@@ -9,9 +9,20 @@ class MustContainAtSymbolError(Exception):
 class InvalidDomainError(Exception):
     pass
 
+
+class MoreThanOneAtSymbolError(Exception):
+    pass
+
+
 # add extension for more
 VALID_DOMAINS = ('com', 'bg', 'org', 'net')
 
+
+def validate_email():
+    pass
+
+
+# def get_email():
 command = input()
 while command != "End":
     email = command
@@ -22,11 +33,26 @@ while command != "End":
     name, domain = email.split('@')
 
     if len(name) <= 4:
-        raise NameTooShortError("Name must be more than 4 characters")
+        raise NameTooShortError("Name must be more than 4 characters!")
     if domain.split('.')[1] not in VALID_DOMAINS:
-        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net!")
+    if email.count('@') > 1:
+        raise MoreThanOneAtSymbolError("Email should contain only one At symbol!")
 
     command = input()
 
+
+def main():
+    pass
+    """
+    (1) get email - get the password - validate each one.
+    
+    (2) add the User to the database (use one for real or something for improvised)
+    
+    (3) let the user to access something interesting for all that work he did
+    passing all the exceptions
+    
+    (4) Show your self the fuck off boy
+    """
 
 
