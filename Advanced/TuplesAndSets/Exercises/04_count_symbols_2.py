010828a083ac71185d4778e49dfee2178f5f52e9
slavcho179 <slavidimitrov06@abv.bg>
2024-02-02 14:23:06 +0200

    change the name of the dir

--- Advanced/TuplesAndSets/Exercises/04_count_symbols_2.py
+++
@@ -1,7 +0,0 @@
-occurrences = {}
-
-for symbol in input():
-    occurrences[symbol] = occurrences.get(symbol, 0) + 1
-
-for symbol, times in sorted(occurrences.items()):
-    print(f"{symbol}: {times} time/s")
