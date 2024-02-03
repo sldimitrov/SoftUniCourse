a51fb3d8b185a8d1ce08f3978a2226eaab1b73d0
slavcho179 <slavidimitrov06@abv.bg>
2024-02-04 00:03:11 +0200

    upload changes

--- Advanced/Exams/python_advanced_exam_13_11/01_worms_and_holes.py
+++ Advanced/Exams/python_advanced_exam_13_11/01_worms_and_holes.py
@@ -1,39 +1,47 @@
 from collections import deque
 
+# Read User input
 worms = [int(x) for x in input().split()]
 apples = deque(int(x) for x in input().split())
 
+# Save the number of worms into a variable
 total_worms = len(worms)
+# Initialise another for these who find a whole for themselves
 worms_with_holes = 0
 
+# Iterate while we have both worms and apples
 while worms and apples:
+    # Take an element from a collection
     curr_worm = worms.pop()
     curr_apple = apples.popleft()
 
+    # check if the worm lower or equal to 0
     if curr_worm <= 0:
-        apples.appendleft(curr_apple)
+        apples.appendleft(curr_apple)  # append the apple to its collection
         continue
 
+    # check if we have a match between the apple and the worm
     if curr_worm == curr_apple:
-        worms_with_holes += 1
+        worms_with_holes += 1  # increase the worm_with_holes count
         continue
-    else:
+    else:  # if we don't have a match append to the list the word decreased by 3
         worms.append(curr_worm - 3)
 
+# Print User output
 if worms_with_holes:
     print(f"Matches: {worms_with_holes}")
-else:
+else:  # if there are no worms with holes
     print("There are no matches.")
 
-if not worms:
-    if total_worms == worms_with_holes:
+if not worms:  # if there are no worms in the list
+    if total_worms == worms_with_holes:  # if total worm count is equal to the count of worms with holes
         print("Every worm found a suitable hole!")
     else:
         print("Worms left: none")
-else:
+else:  # if there are any worms left
     print(f"Worms left: {', '.join(str(x) for x in worms)}")
 
-if apples:
+if apples:  # if there are any apples
     print(f"Holes left: {', '.join(str(x) for x in apples)}")
-else:
+else:  # if there are not
     print("Holes left: none")
--- Advanced/Exams/python_advanced_exam_13_11/02_the_gambler.py
+++ Advanced/Exams/python_advanced_exam_13_11/02_the_gambler.py
@@ -1,23 +1,37 @@
-def valid_coordinates(x, y):
+def valid_coordinates(x: int, y: int) -> bool:
+    """
+    This functions takes: two int params.
+    They are coordinates, and we check if they are in the valid range of our matrix and return: True if so,
+    otherwise return: False.
+    """
     if 0 <= x < size and 0 <= y < size:
         return True
     return False
 
 
+# Read the size of the matrix
 size = int(input())
+
+# Initialise variables for the matrix and for player position
 matrix = []
 player_pos = []
+
+# Initialise variable for the player's initial entering - 100$
 money = 100
 
+# Initialise a for loop to read the matrix
 for row in range(size):
-    line = list(input())
+    line = list(input())  # each line is a list of elements
 
-    matrix.append(line)
+    matrix.append(line)  # append each line to the matrix
 
-    if 'G' in matrix[row]:
+    if 'G' in matrix[row]:  # check if 'G' is in the current row of the matrix
+        # Save the coordinates of the position of the player
         player_pos = [row, matrix[row].index('G')]
+        # Change its symbol with dash in the matrix
         matrix[player_pos[0]][player_pos[1]] = '-'
 
+# Initialise the directions within a dictionary
 directions = {
     "up": [-1, 0],
     "down": [1, 0],
@@ -25,41 +39,51 @@ directions = {
     "right": [0, 1],
 }
 
+# Initialise a boolean to determine if the player had lost or won at the end
 is_lost = False
 
+# Read command until 'end'
 command = input()
 while command != "end":
+    # Taking the coordinates of the future position
     pos_0 = player_pos[0] + directions[command][0]
     pos_1 = player_pos[1] + directions[command][1]
 
+    # Check if the coordinates are in the valid range
     if valid_coordinates(pos_0, pos_1):
+        # Save the future position symbol in a variable
         position = matrix[pos_0][pos_1]
 
-        if position == 'W':
+        # Check which one of these is our symbol
+        if position == 'W':  # Win
             money += 100
-        elif position == 'P':
+        elif position == 'P':  # Penalty
             money -= 200
-        elif position == 'J':
+        elif position == 'J':  # JACKPOT
             print("You win the Jackpot!")
             money += 100000
-            break
+            break  # we break after winning the jackpot
         if money <= 0:
             is_lost = True
-            break
+            break  # we also break if the player run out of money
 
     else:  # invalid coords
         is_lost = True
-        break
+        break  # last but not least, we break if the curr coordinates are invalid
 
+    # We change the last player position with dash
     matrix[player_pos[0]][player_pos[1]] = '-'
+    # Move the player to the current position
     player_pos = [pos_0, pos_1]
 
+    # Read command
     command = input()
 
+# Move the player to the last position after the break
 matrix[pos_0][pos_1] = 'G'
 
-if is_lost:
+if is_lost:  # the player has lost
     print("Game over! You lost everything!")
-else:
+else:  # he has won
     print(f"End of the game. Total amount: {money}$")
     [print(''.join(row)) for row in matrix]
