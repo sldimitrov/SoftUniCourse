9d3288d78cc29dcce3bfcb8c3934ba3badb503c1
slavcho179 <slavidimitrov06@abv.bg>
2024-02-10 01:32:42 +0200

    upload the start of a workshop project

---
+++ Advanced/Exercises/workshop/connect_four_game.py
@@ -0,0 +1,18 @@
+
+def show_the_matrix(matrix):
+    for i in matrix:
+        print(*i)
+
+
+def read_the_matrix():
+    table = [[0 for x in range(7)] for i in range(6)]
+    return table
+
+
+# Initialise the matrix -game table
+play_table = read_the_matrix()
+
+while True:
+
+
+
