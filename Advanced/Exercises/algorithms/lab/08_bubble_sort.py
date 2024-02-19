a9847ee51988985953b9974685349d822fadc198
slavcho179 <slavidimitrov06@abv.bg>
2024-02-19 21:28:01 +0200

    upload bubble sort algorithm

--- Advanced/Exercises/algorithms/lab/08_bubble_sort.py
+++ Advanced/Exercises/algorithms/lab/08_bubble_sort.py
@@ -1,24 +1,22 @@
+# Define a function
+
 def bubble_sort(nums):
     is_sorted = False
-    iterations = 0
+    i = 0
 
     while not is_sorted:
         is_sorted = True
-        for idx in range(len(nums)):
-            for j in range(1, len(nums) - iterations):
-                i = j - 1
-                if nums[j] < nums[i]:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    is_sorted = False
+        for j in range(1, len(nums)):
+            if nums[j - 1] > nums[j]:
+                nums[j - 1], nums[j] = nums[j], nums[j - 1]
+                is_sorted = False
 
-            iterations += 1
+        i += 1
 
     return nums
 
 
 # Read User input
 collection = [int(x) for x in input().split()]
-# Call the function
-sorted_collection = bubble_sort(collection)
-# Print the result
-print(*sorted_collection)
+
+print(*bubble_sort(collection))
