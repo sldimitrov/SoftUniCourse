33ec070ed42580303c6d20f4d381757213d14f7b
slavcho179 <slavidimitrov06@abv.bg>
2024-02-22 00:43:31 +0200

    update workflow and add exercises

--- Advanced/Exercises/modules/mathematical_operations/main.py
+++ Advanced/Exercises/modules/mathematical_operations/main.py
@@ -5,16 +5,19 @@ sequence = input()
 
 # Initialise a mapper dictionary
 sign_mapper = {
-    "/": 'divide',
-    "*": 'multiply',
-    "-": 'subtract',
-    "+": 'addition',
-    "^": 'raising',
+    "/": divide,
+    "*": multiply,
+    "-": subtract,
+    "+": addition,
+    "^": raising,
 }
 
 # Unpack the sequence elements
 num_1, sign, num_2 = sequence.split()
 
-# Find the required function by using the mapper
-required_function = sign_mapper[sign](num_1, num_2)
+# Try to convert the numbers into integer
 
+num_1, num_2 = float(num_1), float(num_2)
+
+
+print(sign_mapper[sign](num_1, num_2) if sign_mapper.get(sign) else "Invalid operator")
