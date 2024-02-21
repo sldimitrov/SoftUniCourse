a482b6faf1ba830de5783a61a57f3be5aa1644f3
slavcho179 <slavidimitrov06@abv.bg>
2024-02-22 00:50:25 +0200

    upload the working pythom package with matematician expression

---
+++ Advanced/Exercises/modules/mathematical_operations/constants.py
@@ -0,0 +1,10 @@
+from core import divide, multiply, subtract, addition, raising
+
+# Initialise a mapper dictionary
+sign_mapper = {
+    "/": divide,
+    "*": multiply,
+    "-": subtract,
+    "+": addition,
+    "^": raising,
+}
--- Advanced/Exercises/modules/mathematical_operations/main.py
+++ Advanced/Exercises/modules/mathematical_operations/main.py
@@ -1,23 +1,16 @@
-from core import divide, multiply, subtract, addition, raising
+from constants import sign_mapper
 
 # Read User input
 sequence = input()
 
-# Initialise a mapper dictionary
-sign_mapper = {
-    "/": divide,
-    "*": multiply,
-    "-": subtract,
-    "+": addition,
-    "^": raising,
-}
-
 # Unpack the sequence elements
 num_1, sign, num_2 = sequence.split()
 
-# Try to convert the numbers into integer
-
+# Try to convert the strings into float values
 num_1, num_2 = float(num_1), float(num_2)
 
+# Find the result of the math sequence
+result = (sign_mapper[sign](num_1, num_2) if sign_mapper.get(sign) else "Invalid operator")
 
-print(sign_mapper[sign](num_1, num_2) if sign_mapper.get(sign) else "Invalid operator")
+# Print Use output
+print(f"{result}")
