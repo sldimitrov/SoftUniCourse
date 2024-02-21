3a218ab060c8b591c3447c2e19683e273cf80ca9
slavcho179 <slavidimitrov06@abv.bg>
2024-02-21 20:24:05 +0200

    add new exercises

--- Advanced/Exercises/modules/mathematical_operations/core.py
+++ Advanced/Exercises/modules/mathematical_operations/core.py
@@ -1,1 +1,24 @@
 
+def divide(a: float, b: float) -> float:
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("You cannot divide by zero!")
+    else:
+        return result
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def addition(a: float, b: float) -> float:
+    return a + b
+
+
+def raising(a: float, b: float) -> float:
+    return a ** b
--- Advanced/Exercises/modules/mathematical_operations/main.py
+++ Advanced/Exercises/modules/mathematical_operations/main.py
@@ -1,1 +1,20 @@
+from core import divide, multiply, subtract, addition, raising
+
+# Read User input
+sequence = input()
+
+# Initialise a mapper dictionary
+sign_mapper = {
+    "/": 'divide',
+    "*": 'multiply',
+    "-": 'subtract',
+    "+": 'addition',
+    "^": 'raising',
+}
+
+# Unpack the sequence elements
+num_1, sign, num_2 = sequence.split()
+
+# Find the required function by using the mapper
+required_function = sign_mapper[sign](num_1, num_2)
 
