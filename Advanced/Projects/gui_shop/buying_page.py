206f722bb7495df58d9218a34f80665f5ccac5ea
slavcho179 <slavidimitrov06@abv.bg>
2024-02-25 22:36:35 +0200

    upload changes to the gui project

--- Advanced/Projects/gui_shop/buying_page.py
+++ Advanced/Projects/gui_shop/buying_page.py
@@ -1,9 +1,9 @@
+from tkinter import Button
+
 from PIL import ImageTk, Image
-
+from canvas import frame, root
 from helpers import clean_screen
-from json import load
-
-from Advanced.Projects.gui_shop.canvas import frame
+from json import load, dump
 
 
 def display_products():
@@ -15,22 +15,53 @@ def display_stock():
     with open("db/products.json", "r") as stock:
         info = load(stock)
 
-    x, y = 150, 50
+    x, y = 50, 100
 
     for item_name, item_info in info.items():
+        # pillow_image = Image.open(item_info["image"])
+        # resized_pillow_image = pillow_image.resize((50, 50))
         item_img = ImageTk.PhotoImage(Image.open(item_info["image"]))
-        images.append(item_img)  # keeping the reference to the image so tkinter
-        # does not delete it after the function ends
+        images.append(item_img)
+        # keeping the reference to the image so that tkinter doesn't delete it after the function ends
 
-        frame.create_text(x, y, text=item_name)
-        frame.create_image(x, y + 100, image=item_img)
+        frame.create_text(x, y, text=item_name, font=("Comic Sans MS", 15))
+        frame.create_image(x, y + 70, image=item_img)
+
+        if item_info["quantity"] > 0:
+            color = "green"
+            text = f"In stock: {item_info['quantity']}"
+
+            item_button = Button(
+                root,
+                text="Buy",
+                bg="green",
+                fg="white",
+                font=("Comic Sans MS", 12),
+                width=50,
+                command=lambda x=item_name, y=info: buy_product(x, y)
+            )
+
+            frame.create_window(x, y + 220, window=item_button)
+        else:
+            color = "red"
+            text = "Out of stock"
+
+        frame.create_text(x, y + 180, text=text, fill=color, font=("Comic Sans MS", 14))
 
         x += 200
 
-        if x >= 550:
+        if x >= 650:
             y += 270
             x = 150
 
 
-images = []
+def buy_product(product_name, info):
+    info[product_name]["quantity"] -= 1
 
+    with open("db/products.json", "w") as stock:
+        dump(info, stock)
+
+    display_products()
+
+
+images = []
