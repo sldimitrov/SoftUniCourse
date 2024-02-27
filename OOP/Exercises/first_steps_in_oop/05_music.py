b30c2760c01151ed9a61b4955a671085cbf9dbe3
slavcho179 <slavidimitrov06@abv.bg>
2024-02-27 20:44:18 +0200

    upload the last exercise from first-step-in-oop-lab

---
+++ OOP/Exercises/first_steps_in_oop/05_music.py
@@ -0,0 +1,18 @@
+# Initialise a Class
+class Music:
+
+    def __init__(self, title: str, artist: str, lyrics: str):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self):
+        return f'This is "{self.title}" from "{self.artist}"'
+
+    def play(self):
+        return self.lyrics
+
+
+song = Music("Title", "Artist", "Lyrics")
+print(song.print_info())
+print(song.play())
