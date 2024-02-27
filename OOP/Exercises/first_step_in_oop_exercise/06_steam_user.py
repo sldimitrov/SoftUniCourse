63e3050466e218bde85bed50d37baeb452d1dd02
slavcho179 <slavidimitrov06@abv.bg>
2024-02-27 21:13:59 +0200

    upload an exercise and initialise a file for the next one

---
+++ OOP/Exercises/first_step_in_oop_exercise/06_steam_user.py
@@ -0,0 +1,32 @@
+class SteamUser:
+    def __init__(self, username: str, games: list):
+        self.username = username
+        self.games = games
+        self.played_hours = 0
+
+    def play(self, game: str, hours: int):
+        if game in self.games:
+            self.played_hours += hours
+            return f"{self.username} is playing {game}"
+        else:
+            return f"{game} is not in library"
+
+    def buy_game(self, game: str):
+        if game not in self.games:
+            self.games.append(game)
+            return f"{self.username} bought {game}"
+        else:
+            return f"{game} is already in your library"
+
+    def status(self):
+        return (f"{self.username} has {len(self.games)} games. "
+                f"Total play time: {self.played_hours}")
+
+
+user = SteamUser("Peter", ["Rainbow Six Siege", "CS:GO", "Fortnite"])
+print(user.play("Fortnite", 3))
+print(user.play("Oxygen Not Included", 5))
+print(user.buy_game("CS:GO"))
+print(user.buy_game("Oxygen Not Included"))
+print(user.play("Oxygen Not Included", 6))
+print(user.status())
