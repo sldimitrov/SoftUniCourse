d32a2d2a75b27cb7f80c99849a242c89009cca32
slavcho179 <slavidimitrov06@abv.bg>
2024-03-02 17:05:01 +0200

    upload 08th project exercise - finished

--- OOP/Exercises/classes_and_objects/more_exercises/08_task/project/user.py
+++ OOP/Exercises/classes_and_objects/more_exercises/08_task/project/user.py
@@ -1,10 +1,13 @@
+
+
+
 class User:
     """
     Information for each of the library users will be stored
     """
-    def __init__(self, user_id: int, user_name: str):
+    def __init__(self, user_id: int, username: str):
         self.user_id = user_id
-        self.user_name = user_name
+        self.username = username
         self.books = []
 
     def info(self):
