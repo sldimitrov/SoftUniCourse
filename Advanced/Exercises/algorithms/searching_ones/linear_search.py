5a9d4ef8b0ac5a8736484aad1d81de43b4b17184
slavcho179 <slavidimitrov06@abv.bg>
2024-03-04 20:16:56 +0200

    upload implementation of linear search

--- Advanced/Exercises/algorithms/searching_ones/linear_search.py
+++ Advanced/Exercises/algorithms/searching_ones/linear_search.py
@@ -1,1 +1,21 @@
-# TODO : ADD
+def search(array, ln, target):
+    # Traversing the array
+    for i in range(ln):
+        if array[i] == target:
+            return i
+    return -1
+
+
+a = [10, 8, 6, 4, 2]
+print("The given array is ", a)
+
+x = 6
+print("Element to be searched is ", x)
+
+length = len(a)  # length of the array
+
+ind = search(a, length, x)
+if ind == -1:
+    print("Element Not Found")
+else:
+    print("Element is at index ", ind)
