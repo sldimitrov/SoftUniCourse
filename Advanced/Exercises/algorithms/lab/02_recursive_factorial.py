72874b5b4d26e7fef1ae0ae43327d3c3def5a300
slavcho179 <slavidimitrov06@abv.bg>
2024-03-06 11:43:27 +0200

    add two implemented solutions of the same exact problem

--- Advanced/Exercises/algorithms/lab/02_recursive_factorial.py
+++ Advanced/Exercises/algorithms/lab/02_recursive_factorial.py
@@ -1,19 +1,41 @@
 
-def recursive_factorial(n: int):
+def linear_factorial(n):
+    """
+    This function returns the factorial of a positive number.
+    :param n: integer number
+    :return: int number
+    """
 
-    # Base case - Ending the recursive call
-    if n == 1:
-        return 1
+    # Initialise a total sum counter
+    total_sum = 1
 
-    # Recursive call - Потапяне
-    total_sum = n * recursive_factorial(n - 1)
+    # Main loop
+    for i in range(n, 1, -1):  # Time complexity - O(n)
+        total_sum *= i
 
-    # Изплуване
+    # Return statement
     return total_sum
 
 
-# Read User input
-number = int(input())
+# Call the function above and pass arg
+print(linear_factorial(5))  # print the returned value
 
-# Call the function and print its result
-print(recursive_factorial(number))
+
+def recursive_factorial(n):
+    """
+    A function that returns the factorial of a number using recursion
+    :param n: int
+    :return: integer number
+    """
+
+    # Base case - Дъно
+    if n == 1:  # Изплуване
+        return 1
+
+    # Recursion case
+    total_sum = n * recursive_factorial(n - 1)  # Потапяне
+
+    return total_sum
+
+
+print(recursive_factorial(5))
