1f91b28a6c0ddf0fb960c80f5175a18b5243e544
slavcho179 <slavidimitrov06@abv.bg>
2024-03-12 16:47:53 +0200

    upload custom implementation of the build in 'reduce' func

---
+++ PythonOOP/Exercises/class_static_methods/project/main.py
@@ -0,0 +1,21 @@
+class Person:
+    min_age = 0
+    max_age = 150
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def __validate_age(cls, value):
+        raise ValueError(f'{value} must be between '
+                         f'{cls.min_age} and {cls.max_age}')
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        self.__validate_age(value)
+        self.__age = value
