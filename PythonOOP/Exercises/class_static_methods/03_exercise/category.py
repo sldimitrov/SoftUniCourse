a171f895b203fb8f4760c147493fb00ef3d9c279
slavcho179 <slavidimitrov06@abv.bg>
2024-03-13 21:01:56 +0200

    add two classes 2/3

---
+++ PythonOOP/Exercises/class_static_methods/03_exercise/category.py
@@ -0,0 +1,13 @@
+
+class Category:
+
+    def __init__(self, _id: int, name: str):
+        self.id = _id
+        self.name = name
+
+    def edit(self, new_name: str):
+        self.name = new_name
+
+    def __repr__(self):
+        return f"Category {self.id}: {self.name}"
+
