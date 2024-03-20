0ac0fa494a5eba7ce6bcad56ea67826a073a9221
slavcho179 <slavidimitrov06@abv.bg>
2024-03-20 11:02:11 +0200

    Upload exercises from SOLID

--- PythonOOP/Exercises/solid/SOLID_exercises/exercise-resources/emails.py
+++ PythonOOP/Exercises/solid/SOLID_exercises/exercise-resources/emails.py
@@ -1,7 +1,28 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
+
+class IContent(ABC):
+    def __init__(self, text: str):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        ...
+
+
+class MyMl(IContent):
+
+    def format(self):
+        return '\n'.join(['<myML>', self.text, '</myML>'])
+
+
+class Im(IContent):
+
+    def format(self):
+        return ''.join(["I'm ", self.text])
+
+
+class IEmail(ABC):
 
     @abstractmethod
     def set_sender(self, sender):
@@ -15,43 +36,34 @@ class IEmail(object):
     def set_content(self, content):
         pass
 
-class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+class Email(IEmail, MyMl, Im):
+
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
 
     def set_sender(self, sender):
-        if self.protocol == 'IM':
-            self.__sender = ''.join(["I'm ", sender])
-        else:
-            self.__sender = sender
+        self.__sender = sender.format()
 
     def set_receiver(self, receiver):
-        if self.protocol == 'IM':
-            self.__receiver = ''.join(["I'm ", receiver])
-        else:
-            self.__receiver = receiver
+        self.__receiver = receiver.format()
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content.format()
 
     def __repr__(self):
 
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
 
-        return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content)
 
 
-email = Email('IM', 'MyML')
-email.set_sender('qmal')
+email = Email('IM')
+email.set_sender('Jamal')
 email.set_receiver('james')
-email.set_content('Hello, there!')
+content = MyMl('Hello, there!')
+email.set_content(content)
 print(email)
-
--- PythonOOP/Exercises/solid/SOLID_exercises/exercise-resources/shapes.py
+++ PythonOOP/Exercises/solid/SOLID_exercises/exercise-resources/shapes.py
@@ -1,25 +1,57 @@
-class Rectangle:
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+
+class Rectangle(Shape):
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
+    def calculate_area(self):
+        return self.width * self.height
+
+
+class Triangle(Shape):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def calculate_area(self):
+        return (self.a * self.b) / 2
+
+
 class AreaCalculator:
 
-    def __init__(self, shapes):
-
-        assert isinstance(shapes, list), "`shapes` should be of type `list`."
+    def __init__(self, shapes: list):
         self.shapes = shapes
+
+    @property
+    def shapes(self):
+        return self.__shapes
+
+    @shapes.setter`
+    def shapes(self, value):
+        if not isinstance(self.shapes, list):
+            raise AssertionError("`shapes` should be of type")
+
+        self.__shapes = value
 
     @property
     def total_area(self):
         total = 0
+
         for shape in self.shapes:
-            total += shape.width * shape.height
+            total += shape.calculate_area()
 
         return total
 
 
-shapes = [Rectangle(2, 3), Rectangle(1, 6)]
+shapes = [Rectangle(2, 3), Rectangle(1, 6), Triangle(2, 3), Triangle(5, 6)]
 calculator = AreaCalculator(shapes)
 print("The total area is: ", calculator.total_area)
